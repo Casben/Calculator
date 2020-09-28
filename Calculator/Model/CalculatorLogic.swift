f9e5ad9ec5ca81542f543ddc711c0d63a8d521cc
HDodge <herbiedodge@gmail.com>
2020-09-28 10:02:57 -0500

    Add operation calculation

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -9,22 +9,49 @@ import Foundation
 
 struct CalculatorLogic {
     
-    var number: Double
+    private var number: Double?
     
-    init(number: Double) {
+    private var intermediateCalculation: (numberOne: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double) {
         self.number = number
     }
     
-    func calculate(symbol: String) -> Double? {
+    mutating func calculate(symbol: String) -> Double? {
         
-        if symbol == "+/-" {
-            return number * -1
-        } else if symbol == "AC" {
-            return 0
-        } else if symbol == "%" {
-            return number * 0.01
+        if let n = number {
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "AC":
+                return 0
+            case "%":
+                return n * 0.01
+            case "=":
+                return performTwoNumberCalculation(n2: n)
+            default:
+                intermediateCalculation = (numberOne: n, calcMethod: symbol)
+            }
         }
-        
+        return nil
+    }
+    
+    private func performTwoNumberCalculation(n2: Double) -> Double? {
+        if let n1 = intermediateCalculation?.numberOne, let operation = intermediateCalculation?.calcMethod {
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("The operation passed in does not match any of the cases")
+                
+            }
+        }
         return nil
     }
 }
